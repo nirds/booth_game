9e9b435bde50382e995deba316ceb1898c3e2842
Patrick Dent <psdent@gmail.com>
2015-12-31 14:32:31 -0800

    entry form

--- app/controllers/contestants_controller.rb
+++ app/controllers/contestants_controller.rb
@@ -1,11 +1,26 @@
 class ContestantsController < ApplicationController
   def new
+    @contestant = Contestant.new
   end
 
   def create
+    @contestant = Contestant.new(contestant_params)
+    if @contestant.save
+      flash[:success] = "Good Luck!"
+      redirect_to new_contestant_path
+    else
+      flash[:notices] = "We couldn't enter you, try again!"
+      redirect_to new_contestant_path
+    end
   end
 
   def index
     @contestants = Contestant.all
+  end
+
+  private
+
+  def contestant_params
+    params.require(:contestant).permit(:name, :email, :twitter_handle)
   end
 end
