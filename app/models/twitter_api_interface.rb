b9e27ff2b2b51cb6c674fb6995512b72fe6a696c
Patrick Dent <psdent@gmail.com>
2016-01-09 16:22:16 -0800

    divide responsibilities into different classes

--- app/models/twitter_api_interface.rb
+++ app/models/twitter_api_interface.rb
@@ -1,33 +1,15 @@
 class TwitterApiInterface
-  require 'net/http'
+  def get_tweets(handle, hash_tag)
+    client = get_client
+    client.search("@#{handle} ##{hash_tag} -rt", { result_type: "recent", count: 100 }).collect
+  end
 
-  def self.search
+  private
+
+  def get_client
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = ENV['TWITTER_KEY']
       config.consumer_secret     = ENV['TWITTER_SECRET']
-    end
-
-    results = client.search("@nirdllc", { result_type: "recent", count: 100 }).collect
-
-    results.each do |tweet|
-      next if tweet.text[0..1] == "RT"
-
-      contestant = Contestant.find_by(twitter_handle: tweet.user.screen_name)
-
-      if contestant
-        unless contestant.tickets.count > 1
-          Ticket::TICKETS_FOR_TWEETING.times { Ticket.create(contestant_id: contestant.id) }
-        end
-
-        if tweet.retweet_count > 0
-          contestant.update_attributes(retweet_count: tweet.retweet_count)
-          pp contestant.retweet_count
-
-          if contestant.tickets.count < 6
-            Ticket::TICKETS_FOR_BEING_RETWEETED.times { Ticket.create(contestant_id: contestant.id) }
-          end
-        end
-      end
     end
   end
 end
