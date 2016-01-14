d6b643f41c80b7d88deaec3e638159bab3e6b07a
Patrick Dent <psdent@gmail.com>
2016-01-14 11:04:21 -0800

    styling in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,1 @@
-game = Game.create(twitter_handle: "beyonce", hash_tag: "lipsyncbattle")
-api = GameUtilities::TwitterApi.new
-tweets = api.get_tweets(game.twitter_handle, game.hash_tag)
-
-tweets.each_with_index do |t, i|
-  unless Contact.find_by(twitter_handle: t.user.screen_name)
-    FactoryGirl.create(:contact, email: "test#{i}@example.com", twitter_handle: t.user.screen_name)
-  end
-end
-
 Admin.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
