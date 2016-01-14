8ed12acf599b67ff95fc7507ae2d628f5ede5e7b
Patrick Dent <psdent@gmail.com>
2016-01-14 09:03:36 -0800

    remove secrets

--- spec/models/game_utilities/twitter_api_spec.rb
+++ spec/models/game_utilities/twitter_api_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe GameUtilities::TwitterApi, :type => :model do
   describe 'get tweets' do
     it 'returns an array of recent tweets' do
-      VCR.use_cassette('beyonce_tweets') do
+      VCR.use_cassette('beyonce_tweets', :match_requests_on => [:headers]) do
         api = GameUtilities::TwitterApi.new
         tweets = api.get_tweets('beyonce', 'lipsyncbattle')
-        first_tweet_text = "I cannot explain how much joy &amp; laughter all of this brought me: https://t.co/ODCITz1F1J #LipSyncBattle @channingtatum @Beyonce @jennaldewan"
+        first_tweet_text = "#lipsyncBattle @channingtatum &amp; @jennaldewan were Xcellent w/ @Beyonce &amp; @PaulaAbdul such an entertaining show @chrissyteigen Hugs Happy 4U!"
         expect(tweets.class).to be(Enumerator)
         expect(tweets.first.text).to eq(first_tweet_text)
       end
