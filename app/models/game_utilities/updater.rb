8212bf34e974eeeb939f560086e6a804db821712
Patrick Dent <psdent@gmail.com>
2016-01-18 16:02:58 -0800

    update game rules, refactor update game logic

--- app/models/game_utilities/updater.rb
+++ app/models/game_utilities/updater.rb
@@ -9,29 +9,17 @@ module GameUtilities
     def award_tickets(all_tweets)
       all_tweets.each do |tweet|
         contact = Contact.find_by(twitter_handle: tweet.user.screen_name)
-        contestant = find_or_create_contestant(contact.id) if contact
-        if contestant && tweet.retweet_count > 0
-          contestant.update_attributes(retweet_count: tweet.retweet_count)
-          award_tickets_for_being_retweeted(contestant.id) if contestant.tickets.count < Ticket.max_ticket_count
-        end
+        next unless contact
+
+        contestant = GameContestant.find_or_create_by(game_id: @game_id, contact_id: contact.id)
+        ticket_hash = { game_contestant_id: contestant.id, game_id: @game_id }
+
+        Ticket.create(ticket_hash) while contestant.tickets.count < Ticket.after_tweeting_count
+        next unless tweet.retweet_count > 0
+
+        contestant.update_attributes(retweet_count: tweet.retweet_count)
+        Ticket.create(ticket_hash) while contestant.tickets.count < Ticket.max_count
       end
-    end
-
-    def find_or_create_contestant(contact_id)
-      contestant = GameContestant.find_by(game_id: @game_id, contact_id: contact_id)
-      unless contestant
-        contestant = GameContestant.create(game_id: @game_id, contact_id: contact_id)
-        award_tickets_for_tweeting(contestant.id)
-      end
-      return contestant
-    end
-
-    def award_tickets_for_tweeting(contestant_id)
-      Ticket::TICKETS_FOR_TWEETING.times{ Ticket.create(game_contestant_id: contestant_id, game_id: @game_id) }
-    end
-
-    def award_tickets_for_being_retweeted(contestant_id)
-      Ticket::TICKETS_FOR_BEING_RETWEETED.times { Ticket.create(game_contestant_id: contestant_id, game_id: @game_id) }
     end
   end
 end
