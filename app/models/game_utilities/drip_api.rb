4fd0027263ccbdf93a538a4720399b5b222bd0b9
Patrick Dent <psdent@gmail.com>
2016-02-13 15:39:28 -0800

    enroll subscribers in campaigns

--- app/models/game_utilities/drip_api.rb
+++ app/models/game_utilities/drip_api.rb
@@ -2,10 +2,15 @@ module GameUtilities
   class DripApi
     def add_or_update_subscribers(game)
       client = get_client
-      ids = GameContestant.where(game_id: game.id).map { |c| c.contact_id }
-      contacts = Contact.where(id: ids)
-      contacts_as_subscribers = contacts.map { |c| {email: c.email }}
-      client.create_or_update_subscribers(contacts_as_subscribers)
+      client.create_or_update_subscribers(contestants_as_subscribers(game))
+    end
+
+    def enroll_contestants_in_campaign(game, campaign_id)
+      client = get_client
+      subscribers = contestants_as_subscribers(game)
+      subscribers.each do |s|
+        client.subscribe(s[:email], campaign_id)
+      end
     end
 
     private
@@ -16,6 +21,11 @@ module GameUtilities
         c.account_id    = ENV['DRIP_ACCT_ID']
       end
     end
+
+    def contestants_as_subscribers(game)
+      contact_ids = GameContestant.where(game_id: game.id).map { |c| c.contact_id }
+      contacts = Contact.where(id: contact_ids)
+      contacts_as_subscribers = contacts.map { |c| {email: c.email }}
+    end
   end
 end
-
