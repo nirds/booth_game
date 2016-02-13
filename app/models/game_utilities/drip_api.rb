ed4db0d6b7f057ee0ff1b4eac5874d26230d0c70
Patrick Dent <psdent@gmail.com>
2016-02-13 15:06:47 -0800

    method to add contestants of a game as drip subscribers

--- app/models/game_utilities/drip_api.rb
+++ app/models/game_utilities/drip_api.rb
@@ -1,9 +1,14 @@
 module GameUtilities
   class DripApi
-    def some_method
+    def add_or_update_subscribers(game)
+      client = get_client
+      ids = GameContestant.where(game_id: game.id).map { |c| c.contact_id }
+      contacts = Contact.where(id: ids)
+      contacts_as_subscribers = contacts.map { |c| {email: c.email }}
+      client.create_or_update_subscribers(contacts_as_subscribers)
     end
 
-    # private
+    private
 
     def get_client
       client = Drip::Client.new do |c|
@@ -13,3 +18,4 @@ module GameUtilities
     end
   end
 end
+
